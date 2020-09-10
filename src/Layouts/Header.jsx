c03e3f778f0535400bf5a46571d497fdc20c1a50
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2020-09-11 01:55:49 +0700

    Implement Complex navbar

---
+++ src/Layouts/Header.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import './Styles.css'
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { Avatar, IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+
+const Header =()=>{
+    return(
+        <nav className='header'>
+            <div className="header-left">
+                <img src={logo} alt='logo' />
+                <div className='header-input'>
+                    <SearchIcon/>
+                    <input type='text' placeholder=' search...' ></input>
+                </div>
+            </div>
+            <div className="header-middle">
+                <div className='icons'>
+                    <HomeIcon fontSize='large'/>
+                </div>
+                <div className='icons'>
+                    <FlagIcon fontSize='large'/>
+                </div>
+                <div className='icons'>
+                    <SubscriptionsIcon fontSize='large'/>
+                </div>
+                <div className='icons'>
+                    <StorefrontIcon fontSize='large'/>
+                </div>
+                <div className='icons'>
+                    <SupervisedUserCircleIcon fontSize='large'/>
+                </div>
+            </div>
+            <div className="header-right">
+                <div className='header-info'>
+                    <Avatar src={logo}/>
+                    <h4>Mohamed </h4>
+                    
+                </div>
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
+                <IconButton>
+                <ExpandMoreIcon />
+                </IconButton>
+            </div>
+        </nav>
+    )
+}
+export default Header
